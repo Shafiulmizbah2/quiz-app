12c29a37fbcb389d1f293b247141a022dbf6bbe3
Shafiul Azim <shafiulmizbah2@gmail.com>
2022-07-08 20:19:50 +0600

    add some unit test

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
+# Add a .env file to the root folder and add
+
+REACT_APP_ENVIRONMENT = dev OR production
+REACT_APP_BASE_URL = https://opentdb.com
+
 # Getting Started with Create React App and Redux
 
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app), using the [Redux](https://redux.js.org/) and [Redux Toolkit](https://redux-toolkit.js.org/) template.
--- src/App.test.js
+++ src/App.test.js
@@ -1,15 +1,63 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import { Provider } from 'react-redux';
-import { store } from './app/store';
-import App from './App';
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import Nav from "./components/Nav";
+import SelectField from "./components/SelectField";
+import CusTextField from "./components/CusTextField";
+import CusRadioButtonsGroup from "./components/CusRadioButtonGroup";
+import { Provider } from "react-redux";
+import { store } from "./store";
+import { BrowserRouter } from "react-router-dom";
 
-test('renders learn react link', () => {
-  const { getByText } = render(
+it("If it render No question found", () => {
+  render(
     <Provider store={store}>
-      <App />
+      <CusRadioButtonsGroup />
     </Provider>
   );
+  expect(screen.findByRole("h3", /No question found!/i)).toBeDefined();
+});
 
-  expect(getByText(/learn/i)).toBeInTheDocument();
+it("If it render nav bar", () => {
+  render(
+    <Provider store={store}>
+      <BrowserRouter>
+        <Nav />
+      </BrowserRouter>
+    </Provider>
+  );
+  expect(screen.getByText(/Quiz App/i)).toBeDefined();
+});
+
+it("If it render select field", () => {
+  render(
+    <Provider store={store}>
+      <SelectField
+        label="Test label"
+        value="test value"
+        name="test field"
+        options={[
+          { id: "easy", name: "Easy" },
+          { id: "medium", name: "Medium" },
+          { id: "hard", name: "Hard" },
+        ]}
+        handleChange={() => null}
+      />
+    </Provider>
+  );
+  expect(screen.findByText(/Test label/i)).toBeDefined();
+});
+
+it("If it render text field", () => {
+  render(
+    <Provider store={store}>
+      <CusTextField
+        label="Text field"
+        value={3}
+        name="test Text field"
+        handleChange={() => null}
+      />
+    </Provider>
+  );
+  expect(screen.findByDisplayValue(/3/i)).toBeDefined();
+  expect(screen.findByDisplayValue(/Text field/i)).toBeDefined();
 });
